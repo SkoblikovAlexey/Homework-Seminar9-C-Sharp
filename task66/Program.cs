3face1eacca017c1e30ff56dc6bae2c20c0c68ec
Alexey Skoblikov <happycow32@yandex.ru>
2022-09-05 22:37:13 +0300

    Task 66

---
+++ task66/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Number(int M, int N)
+{
+    int result = 0;
+    if(M <= N) return result += M + Number(M + 1, N);
+    else return 0;
+}
+
+Console.WriteLine(Number(1, 15));
