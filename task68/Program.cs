1df705949eb72ee78d27f56968c4f7c0d0486156
Alexey Skoblikov <happycow32@yandex.ru>
2022-09-06 22:59:26 +0300

    Task 68

---
+++ task68/Program.cs
@@ -0,0 +1,16 @@
+﻿int A(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m != 0 && n == 0)
+        return A(m - 1, 1);
+    else 
+        return A(m - 1, A(m, n - 1));
+}     
+void Main()
+{
+    int k;
+    k = A(2, 3); 
+    Console.WriteLine(k);
+}
+Main();
